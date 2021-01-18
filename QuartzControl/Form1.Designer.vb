afcc769363db1790cfdaeaf215f3e7594a70ec56
lizhao <lizhaoiot@163.com>
2021-01-18 11:08:46 +0800

    第一版

---
+++ QuartzControl/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿Partial Public Class Form1
+    ''' <summary>
+    ''' Required designer variable.
+    ''' </summary>
+    Private components As System.ComponentModel.IContainer = Nothing
+
+    ''' <summary>
+    ''' Clean up any resources being used.
+    ''' </summary>
+    ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso (components IsNot Nothing) Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+#Region "Windows Form Designer generated code"
+
+    ''' <summary>
+    ''' Required method for Designer support - do not modify
+    ''' the contents of this method with the code editor.
+    ''' </summary>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ribbonControl1 = New DevExpress.XtraBars.Ribbon.RibbonControl()
+        Me.ribbonPage1 = New DevExpress.XtraBars.Ribbon.RibbonPage()
+        Me.ribbonPageGroup1 = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        CType(Me.ribbonControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ribbonControl1
+        '
+        Me.ribbonControl1.ExpandCollapseItem.Id = 0
+        Me.ribbonControl1.Items.AddRange(New DevExpress.XtraBars.BarItem() {Me.ribbonControl1.ExpandCollapseItem})
+        Me.ribbonControl1.Location = New System.Drawing.Point(0, 0)
+        Me.ribbonControl1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ribbonControl1.MaxItemId = 1
+        Me.ribbonControl1.Name = "ribbonControl1"
+        Me.ribbonControl1.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() {Me.ribbonPage1})
+        Me.ribbonControl1.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2010
+        Me.ribbonControl1.Size = New System.Drawing.Size(875, 184)
+        '
+        'ribbonPage1
+        '
+        Me.ribbonPage1.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() {Me.ribbonPageGroup1})
+        Me.ribbonPage1.Name = "ribbonPage1"
+        Me.ribbonPage1.Text = "ribbonPage1"
+        '
+        'ribbonPageGroup1
+        '
+        Me.ribbonPageGroup1.Name = "ribbonPageGroup1"
+        Me.ribbonPageGroup1.Text = "ribbonPageGroup1"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AllowFormGlass = DevExpress.Utils.DefaultBoolean.[False]
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(875, 566)
+        Me.Controls.Add(Me.ribbonControl1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Ribbon = Me.ribbonControl1
+        Me.Text = "Form1"
+        CType(Me.ribbonControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+#End Region
+
+    Private WithEvents ribbonControl1 As DevExpress.XtraBars.Ribbon.RibbonControl
+    Private ribbonPage1 As DevExpress.XtraBars.Ribbon.RibbonPage
+    Private ribbonPageGroup1 As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ QuartzControl/Form1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+
+
+Partial Public Class Form1
+    Inherits DevExpress.XtraBars.Ribbon.RibbonForm
+    Shared Sub New()
+        DevExpress.UserSkins.BonusSkins.Register()
+        DevExpress.Skins.SkinManager.EnableFormSkins()
+    End Sub
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+End Class
